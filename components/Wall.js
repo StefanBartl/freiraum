012b44ad674868fbc5795e4f60f1345bd7e760cc
Steve B <wkdatabase@gmail.com>
2022-08-13 06:09:15 +0200

    Wall extend

--- components/Wall.js
+++ components/Wall.js
@@ -1,20 +1,19 @@
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
-export default function Wall(props){
+export default function Wall(props) {
+  return (
+    <section className={`${styles.wall}`}>
+      <div className={`${styles.newpost}`}>
+        <img src="./graphics/images/sonnenbild.jpg" alt="Profilbild"></img>
+        <button type="text" title="Schreib einen Post">
+          Was machst du gerade?
+        </button>
+      </div>
 
-    return (
-        <section className={`${styles.wall}`}>
-
-            <div className={`${styles.newpost}`}>
-                <img src='./graphics/images/sonnenbild.jpg' alt='Profilbild' ></img>
-                <button type='text' title='Schreib einen Post'>Was machst du gerade?</button>
-            </div>
-
-            <div className={`${styles.currentwall}`}>
-                <h2>Wall</h2>
-            </div>
-
-        </section>
-    )
-};
+      <div className={`${styles.currentwall}`}>
+        <h2>Wall</h2>
+      </div>
+    </section>
+  );
+}
